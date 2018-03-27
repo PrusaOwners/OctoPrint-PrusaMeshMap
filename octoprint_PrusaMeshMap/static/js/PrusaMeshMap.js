57a2784eceea3ca2db2996d6333d3537368a09e9
ff8jake <ff8jake@gmail.com>
2018-03-26 21:44:05 -0500

    Adding initial files.

---
+++ octoprint_PrusaMeshMap/static/js/PrusaMeshMap.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-PrusaMeshMap
+ *
+ * Author: Jake Skipper
+ * License: MIT
+ */
+$(function() {
+    function PrusameshmapViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: PrusameshmapViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_PrusaMeshMap, #tab_plugin_PrusaMeshMap, ...
+        elements: [ /* ... */ ]
+    });
+});
