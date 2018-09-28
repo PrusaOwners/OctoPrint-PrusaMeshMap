ff02afabab96a76f95ce46705ada3dbe50cfbb8e
mcm001 <matthew.morley.ca@gmail.com>
2018-09-28 14:13:32 -0700

    Testing Mode

--- octoprint_PrusaMeshMap/__init__.py
+++ octoprint_PrusaMeshMap/__init__.py
@@ -30,11 +30,14 @@ class PrusameshmapPlugin(octoprint.plugin.SettingsPlugin,
 
     ##~~ SettingsPlugin mixin
 
+    self.pluginTestingMode = 'True'
+
     def get_settings_defaults(self):
         return dict(
             do_level_gcode = 'G28 W ; home all without mesh bed level\nG80 ; mesh bed leveling\nG81 ; check mesh leveling results',
             matplotlib_heatmap_theme = 'viridis',
             matplotlib_heatmap_background_image_style = 'MK52 Mode'
+            
         )
 
     ##~~ AssetPlugin mixin
@@ -100,8 +103,13 @@ class PrusameshmapPlugin(octoprint.plugin.SettingsPlugin,
             klipper_json_line = line
             self.generate_graph_klipper_mode(klipper_json_line)
             return line
+        elif (self.pluginTestingMode == 'True') and (line.startswith('ok')):
+            self._logger.info("Processing in Testing mode")
+            klipper_json_line = line
+            self.generate_graph_klipper_mode(klipper_json_line)
         else:
             return line
+
 
         # Klipper mode heatmap generation. Above brig's stuff because it's better :D
 
@@ -110,7 +118,8 @@ class PrusameshmapPlugin(octoprint.plugin.SettingsPlugin,
         self._logger.info("Processing in Klipper mode...")
         #Remove the first 16 charicters of the line and import to a dictionary
         #use this line for testing
-        #klipper_json_line = 'mesh_map_output {"max_point": [212.0, 204.0], "z_positions": [[-0.12499999999999833, -0.10999999999999777, -0.09750000000000203, -0.1399999999999989, -0.2200000000000043], [0.014999999999995128, 0.02749999999999797, 0.012499999999997402, -0.016249999999997766, -0.0500000000000026], [0.02749999999999797, 0.03250000000000053, 0.02875000000000394, 0.046250000000002234, 0.034999999999998255], [0.04000000000000081, 0.07750000000000223, 0.07999999999999996, 0.0787500000000011, 0.08124999999999882], [-0.01000000000000345, 0.05249999999999655, 0.11750000000000138, 0.11750000000000138, 0.10250000000000081]], "xy_offset": [24.0, 5.0], "min_point": [2.0, 0.0]}'
+        if self.pluginTestingMode == 'True':
+            klipper_json_line = 'mesh_map_output {"max_point": [212.0, 204.0], "z_positions": [[-0.12499999999999833, -0.10999999999999777, -0.09750000000000203, -0.1399999999999989, -0.2200000000000043], [0.014999999999995128, 0.02749999999999797, 0.012499999999997402, -0.016249999999997766, -0.0500000000000026], [0.02749999999999797, 0.03250000000000053, 0.02875000000000394, 0.046250000000002234, 0.034999999999998255], [0.04000000000000081, 0.07750000000000223, 0.07999999999999996, 0.0787500000000011, 0.08124999999999882], [-0.01000000000000345, 0.05249999999999655, 0.11750000000000138, 0.11750000000000138, 0.10250000000000081]], "xy_offset": [24.0, 5.0], "min_point": [2.0, 0.0]}'
         klipper_json_line = klipper_json_line[16:]
         jsonDict = json.loads(klipper_json_line)
         xyOffset = jsonDict["xy_offset"]
@@ -125,14 +134,14 @@ class PrusameshmapPlugin(octoprint.plugin.SettingsPlugin,
         z_positions= np.array(jsonDict["z_positions"])
         z_positions_shape = z_positions.shape
         minMax = [minPoints[0],maxPoints[0],minPoints[1],maxPoints[1]]
-        probeSpacingX = (maxPoints[0]-minPoints[0])/(z_positions_shape[1]-1)
-        probeSpacingY = (maxPoints[1]-minPoints[1])/(z_positions_shape[0]-1)
+        #probeSpacingX = (maxPoints[0]-minPoints[0])/(z_positions_shape[1]-1)
+        #probeSpacingY = (maxPoints[1]-minPoints[1])/(z_positions_shape[0]-1)
 
         #Variables shamelessly reused from the stock FW code
         BED_SIZE_X = 250
         BED_SIZE_Y = 210
-        BED_PRINT_ZERO_REF_X = 2
-        BED_PRINT_ZERO_REF_Y = 9.4
+        #BED_PRINT_ZERO_REF_X = 2
+        #BED_PRINT_ZERO_REF_Y = 9.4
         SHEET_OFFS_X = 0
         SHEET_OFFS_Y = 0
         SHEET_MARGIN_LEFT = 0
